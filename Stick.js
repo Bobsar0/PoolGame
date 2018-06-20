33df93ae94e59a6ef7c501012ea8896c11e69235
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-06-20 05:22:27 +0100

    Stick moves backwards to shoot ball

--- Stick.js
+++ Stick.js
@@ -4,15 +4,20 @@ const STICK_ORIGIN = new Vector2(970, 11);
 function Stick(position){
     this.position = position; //Get the position of the stick from outside
     this.rotation = 0;  // Initializes Stick rotation
+    this.origin = STICK_ORIGIN.copy(); //Copy of stick origin
+    this.power = 0;  //Initializes shot power
 }
 
 Stick.prototype.update = function(){
+    if (Mouse.left.down){
+        this.increasePower();
+    }
     this.updateRotation();
 }
 
 //Method to draw the stick
 Stick.prototype.draw = function(){
-    Canvas.drawImage(sprites.stick, this.position, STICK_ORIGIN, this.rotation);
+    Canvas.drawImage(sprites.stick, this.position, this.origin, this.rotation);
 }
 
 //Method to update the rotation of the stick upon mouse movement
@@ -21,4 +26,9 @@ Stick.prototype.updateRotation = function(){
     let adjacent = Mouse.position.x - this.position.x;
 
     this.rotation = Math.atan2(opposite, adjacent); //Gets angle of rotation
+}
+
+Stick.prototype.increasePower = function(){
+    this.power += 100;
+    this.origin.x += 5;
 }
--- Vector2.js
+++ Vector2.js
@@ -1,6 +1,10 @@
 //Function that deals with vector arithmetic operations
-function Vector2(x, y){
+function Vector2(x = 0, y = 0){
 
-    this.x = typeof x !== 'undefined' ? x : 0;
-    this.y = typeof y !== 'undefined' ? y : 0;
+    this.x = x;
+    this.y = y;
+}
+
+Vector2.prototype.copy = function(){
+    return new Vector2(this.x, this.y);
 }
