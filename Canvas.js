f91842941ae5004e2ac34ed98be81edffe38fe1c
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-06-20 05:00:01 +0100

    Stick Rotating behind Ball

--- Canvas.js
+++ Canvas.js
@@ -4,34 +4,26 @@ function Canvas2D(){
     this._canvasContext = this._canvas.getContext('2d') //treats the position of the image as the top left corner of the image
 }
 
-//Prototype function to clear the canvas
+//Method to clear the canvas
 Canvas2D.prototype.clear = function(){
     this._canvasContext.clearRect(0, 0, this._canvas.width, this._canvas.height);
 }
 
 //Method to draw image
-Canvas2D.prototype.drawImage = function(image, position, origin){
+Canvas2D.prototype.drawImage = function(image, position, origin, rotate=0){
 
     if(!position){ // if the position of the image is null or 'undefined'
-        position = new Vector2();
+        position = new Vector2(); //set x, y values to be zeroes
     }
     if(!origin){ //If the origin of the image is null or 'undefined'
-        origin = new Vector2();
+        origin = new Vector2(); //set x, y, values to be zeroes
     }
     this._canvasContext.save(); //saves the current state of the context
     this._canvasContext.translate(position.x, position.y); //translates the context to a different position
-    this._canvasContext.drawImage(image, -origin.x, -origin.y); //draw the image with the top left corner position passed in as origin. COntext will now treat the position of the image as defined by origin. This prevents the mouse arrow from being on the left corner of the stick
-    this._canvasContext.restore(); //restore the context to the last saved state
-
+    this._canvasContext.rotate(rotate);  //Rotates the canvas context
+    this._canvasContext.drawImage(image, -origin.x, -origin.y); //draw the image with the top left corner position passed in as origin. Context will now treat the position of the image as defined by origin. This prevents the mouse arrow from being on the left corner of the stick
+    this._canvasContext.restore(); //restores the context to the last saved state
 }
 
 //Object using constructor
 let Canvas = new Canvas2D();
-
-// //For testing
-// let image = new Image();
-// image.src = "./assets/imgs/spr_background5.png"; //location of the image
-
-// setTimeout(() => { //needed because image doesn't load immediately
-//     Canvas.drawImage(image, {x:0, y:0});
-// }, 1000);
--- Stick.js
+++ Stick.js
@@ -3,20 +3,22 @@ const STICK_ORIGIN = new Vector2(970, 11);
 
 function Stick(position){
     this.position = position; //Get the position of the stick from outside
+    this.rotation = 0;  // Initializes Stick rotation
 }
 
 Stick.prototype.update = function(){
-  
-    // //For testing
-    // this.position = Mouse.position;
-
-    // if(Mouse.left.pressed){
-    //     console.log("Pressed left")
-    // }
-
+    this.updateRotation();
 }
 
 //Method to draw the stick
 Stick.prototype.draw = function(){
-    Canvas.drawImage(sprites.stick, this.position, STICK_ORIGIN);
+    Canvas.drawImage(sprites.stick, this.position, STICK_ORIGIN, this.rotation);
+}
+
+//Method to update the rotation of the stick upon mouse movement
+Stick.prototype.updateRotation = function(){
+    let opposite = Mouse.position.y - this.position.y;
+    let adjacent = Mouse.position.x - this.position.x;
+
+    this.rotation = Math.atan2(opposite, adjacent); //Gets angle of rotation
 }
