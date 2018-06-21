bf954053f5b9477330305dbd4ce4d0287cc3c356
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-06-21 04:40:09 +0100

    All Balls loaded

--- GameWorld.js
+++ GameWorld.js
@@ -4,7 +4,27 @@
 const DELTA = 1/100; //represents how mamy times an update occurs on each iteration. 
 
 function GameWorld(){ 
-    this.whiteBall = new Ball(new Vector2(413, 413), COLOR.RED);
+    //Array that contains all the arguments needed to create the Ball objects
+    this.balls = [
+        [new Vector2(1022,413), COLOR.YELLOW], //1
+        [new Vector2(1056,393), COLOR.YELLOW], //2
+        [new Vector2(1056,433), COLOR.RED], //3
+        [new Vector2(1090,374), COLOR.RED], //4
+        [new Vector2(1090,413), COLOR.BLACK], //5
+        [new Vector2(1090,452), COLOR.YELLOW], //6
+        [new Vector2(1126,354), COLOR.YELLOW], //7
+        [new Vector2(1126,393), COLOR.RED], //8
+        [new Vector2(1126,433), COLOR.YELLOW], //9
+        [new Vector2(1126,472), COLOR.RED], //10
+        [new Vector2(1162,335), COLOR.RED], //11
+        [new Vector2(1162,374), COLOR.RED], //12
+        [new Vector2(1162,413), COLOR.YELLOW], //13
+        [new Vector2(1162,452), COLOR.RED], //14
+        [new Vector2(1162,491), COLOR.YELLOW], //15
+        [new Vector2(413,413), COLOR.WHITE]
+    ].map(params => new Ball(params[0], params[1]))
+
+    this.whiteBall = this.balls[this.balls.length - 1]; //the last ball
     this.stick = new Stick(
         new Vector2(413, 413), 
         this.whiteBall.shoot.bind(this.whiteBall) //makes sure we don't lose the reference to the white ball
@@ -13,7 +33,11 @@ function GameWorld(){
 
 GameWorld.prototype.update = function(){
     this.stick.update();
-    this.whiteBall.update(DELTA); //The DELTA is passed in as a fraction that will update the ball just at that fraction of the velocity on every iteration
+    
+    //Loop to update each ball
+    for (let i = 0; i <this.balls.length; i++){
+        this.balls[i].update(DELTA);//The DELTA is passed in as a fraction that will update the ball just at that fraction of the velocity on every iteration
+    }
 
     if(!this.ballMoving() && this.stick.shot){ //If the ball is not moving after stick has been shot
         this.stick.reposition(this.whiteBall.position); //reposition the stick to the new position of the white ball
@@ -23,7 +47,10 @@ GameWorld.prototype.update = function(){
 GameWorld.prototype.draw = function(){
     Canvas.drawImage(sprites.background, {x:0, y:0}); //Draws the background image
     this.stick.draw();
-    this.whiteBall.draw();
+    for (let i = 0; i <this.balls.length; i++){
+        this.balls[i].draw(DELTA);//The DELTA is passed in as a fraction that will update the ball just at that fraction of the velocity on every iteration
+    }
+    
 }
 
 GameWorld.prototype.ballMoving = function(){
